240f5ed21d3a1145a19c7d32c24dea5092e6b454
chaitanya <vchaitanyapraveenreddy@gmail.com>
2026-03-21 22:35:39 +0530

    sql

--- sql/leetcode_185.sql
+++ sql/leetcode_185.sql
@@ -1,10 +1,8 @@
-/*A company's executives are interested in seeing who earns the most money in each of the company's departments. 
-A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+/*A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
 
 Write a solution to find the employees who are high earners in each of the departments.
 
 Return the result table in any order.*/
-
 
 with tmp as
 (
--- sql/leetcode_3793.sql
+++ sql/leetcode_3793.sql
@@ -1,10 +1,10 @@
--- Write a solution to analyze AI prompt usage patterns based on the following requirements:
+/*Write a solution to analyze AI prompt usage patterns based on the following requirements:
 
--- For each user, calculate the total number of prompts they have submitted.
--- For each user, calculate the average tokens used per prompt (Rounded to 2 decimal places).
--- Only include users who have submitted at least 3 prompts.
--- Only include users who have submitted at least one prompt with tokens greater than their own average token usage.
--- Return the result table ordered by average tokens in descending order, and then by user_id in ascending order.
+For each user, calculate the total number of prompts they have submitted.
+For each user, calculate the average tokens used per prompt (Rounded to 2 decimal places).
+Only include users who have submitted at least 3 prompts.
+Only include users who have submitted at least one prompt with tokens greater than their own average token usage.
+Return the result table ordered by average tokens in descending order, and then by user_id in ascending order.*/
 
 with cte as
 (
@@ -12,9 +12,11 @@ with cte as
     round(avg(tokens),2) as avg_tokens, 
     max(tokens) as max_token
     from prompts
-    group by user_id having count(user_id) >= 3
+    group by user_id 
+    having count(user_id) >= 3
     order by avg_tokens desc, user_id
 )
 
 select user_id, prompt_count, avg_tokens 
-from cte where max_token > avg_tokens
+from cte 
+where max_token > avg_tokens
